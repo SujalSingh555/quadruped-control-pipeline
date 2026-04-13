f414448e292015965ec98ea3e2d9b4c3d88c07f6
Sujal Singh <sujaldpsdps@gmail.com>
2026-04-14 00:38:57 +0530

    converted the angular offset of thigh servos

--- comms/sender.py
+++ comms/sender.py
@@ -4,7 +4,12 @@ import numpy as np
 
 def send(joint_array):
     global log_data
-
+    for i in range(int((len(joint_array)/3))):    #angles are taken from -pi/2 to body and clockwise converted to positive sign 
+        joint_array[3*i+1]=-1*(joint_array[3*i+1]+90)
+    #joint_array[1]=-1*(joint_array[1]+90)
+    #joint_array[4]=-1*(joint_array[4]+90)
+    #joint_array[7]=-1*(joint_array[7]+90) 
+    #joint_array[10]=-1*(joint_array[10]+90)   
     print("Sending:", joint_array)
 
     
