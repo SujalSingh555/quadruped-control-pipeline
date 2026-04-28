6b331923a762473ab9d4641bbc5c0a27c4ce64dd
Sujal Singh <sujaldpsdps@gmail.com>
2026-04-28 22:40:18 +0530

    ccommunication to ide enabled

--- comms/sender.py
+++ comms/sender.py
@@ -1,14 +1,24 @@
-# comms/sender.py
-
 import numpy as np
+import serial
+import time
+
+# Initialize once (outside function ideally)
+arduino = serial.Serial('COM3', 9600)   # change COM port
+time.sleep(2)
 
 def send(joint_array):
-    global log_data
-    for i in range(int((len(joint_array)/3))):    #angles are taken from -pi/2 to body and clockwise converted to positive sign 
-        joint_array[3*i+1]=-1*(joint_array[3*i+1]+90)
-    #joint_array[1]=-1*(joint_array[1]+90)
-    #joint_array[4]=-1*(joint_array[4]+90)
-    #joint_array[7]=-1*(joint_array[7]+90) 
-    #joint_array[10]=-1*(joint_array[10]+90)   
-    print("Sending:", joint_array)
-    
+    for i in range(int(len(joint_array)/3)):
+        joint_array[3*i+1] = -1*(joint_array[3*i+1] + 90)
+
+    print("s", joint_array)
+
+    a = joint_array[1] + 37
+    b = joint_array[2] - 90 + 55
+
+    angle = [a, b]
+
+    # 🔥 Convert to string and send
+    data_str = f"{angle[0]},{angle[1]}\n"
+    arduino.write(data_str.encode())
+
+    print("Sent:", data_str)
